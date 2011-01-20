8a463ce65bf5b88eb94278b44087c4446b94c760
Eddie Lau <tatonlto@gmail.com>
2011-01-20 20:00:33 +0800

    no need to call db to retrieve objects

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,15 +1,15 @@
 class MapController < ApplicationController
   def index
-    @districts = District.all
+    # @districts = District.all
     # @subdistricts = Subdistrict.all
-    @recycle_bins_markers = RecycleBin.all.map{|bin| 
-      {
-        :id => bin.id,
-        :add => bin.add,
-        :add_eng => bin.add_eng,
-        :lat => bin.lat,
-        :long => bin.long
-      }
-    }
+    # @recycle_bins_markers = RecycleBin.all.map{|bin| 
+    #   {
+    #     :id => bin.id,
+    #     :add => bin.add,
+    #     :add_eng => bin.add_eng,
+    #     :lat => bin.lat,
+    #     :long => bin.long
+    #   }
+    # }
   end
 end
